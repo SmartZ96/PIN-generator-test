7f7ef5d088efbfe2f07e916b1a1edb7d17961730
SmartZ96 <seansmart26@hotmail.co.uk>
2021-04-11 23:55:59 +0100

    Add files via upload

---
+++ test.js
@@ -0,0 +1,7 @@
+function pinGen() {
+
+    //alert("Your PIN is generating and deliverd in the console");//
+
+   /* generates a 4 digit random number */
+    console.log(Math.floor((Math.random() * 9000) + 999))
+}
